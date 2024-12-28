92ff18e6abc98a15765f72ab202684acbf67db9f
unknown <kukkar_vishal@yahoo.com>
2024-12-28 12:04:29 -0800

    add server send events api

--- springcrud/src/main/java/com/springcrud/Controller/ApiController.java
+++ springcrud/src/main/java/com/springcrud/Controller/ApiController.java
@@ -7,11 +7,13 @@ import com.springcrud.Repo.UserRepo;
 
 import jakarta.websocket.server.PathParam;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
+import reactor.core.publisher.Flux;
 
 
 @RestController
@@ -83,6 +84,13 @@ public class ApiController {
                     return ResponseEntity.ok(true);
                 })
                 .orElseGet(() -> ResponseEntity.badRequest().body(false));
+    }
+
+    @GetMapping(value = "/sse-stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamEvents() {
+        // Generate a stream of data
+        return Flux.interval(Duration.ofSeconds(1))
+                   .map(sequence -> "Event number: " + sequence);
     }
     
 }
